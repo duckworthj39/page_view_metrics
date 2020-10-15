778cee227b4ac8d0a472575fcbd6b69a7cea52b7
Jason Duckworth <duckworthj39@gmail.com>
2020-10-15 07:56:02 +0100

    added validation to PageMetrics

--- lib/page_metrics.rb
+++ lib/page_metrics.rb
@@ -5,6 +5,8 @@ require_relative 'page'
 # PageMetrics provides a method of returning a Hash of Pages which can be used to display the metrics
 class PageMetrics
 
+  class WrongFormatError < StandardError; end
+
   def initialize(log)
     @log = log
   end
@@ -12,10 +14,11 @@ class PageMetrics
   def call
     log.each_with_object({}) do |logged_page, pages|
       log_path, log_user = logged_page.split(' ')
+      validate_logs(log_path, log_user)
+
       next pages[log_path].add_user(log_user) unless pages[log_path].nil?
 
       page = Page.new(log_path)
-
       page.add_user(log_user)
       pages[log_path] = page
     end.values
@@ -24,4 +27,8 @@ class PageMetrics
   private
 
   attr_reader :log
+
+  def validate_logs(log_path, log_user)
+    raise WrongFormatError, 'log file cannot be parsed' if log_path.nil? || log_user.nil?
+  end
 end
--- spec/page_metrics_spec.rb
+++ spec/page_metrics_spec.rb
@@ -14,4 +14,11 @@ RSpec.describe PageMetrics do
     expect(metrics.first.visits).to eq(1)
     expect(metrics.first.unique_visits).to eq(1)
   end
+
+  it 'returns an array of page objects' do
+    logs = File.open('spec/support/incorrect_data.log').read
+    prepared_logs = logs.split("\n")
+
+    expect{ PageMetrics.new(prepared_logs).call }.to raise_error(PageMetrics::WrongFormatError)
+  end
 end
